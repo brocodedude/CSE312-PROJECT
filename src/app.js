61b0b1d5491ddc90843513e3a0331f21e866b888
brocodedude <mofagbam@buffalo.edu>
2024-03-06 15:46:41 -0500

    Add headers for routes & change port to 8080

--- src/app.js
+++ src/app.js
@@ -15,7 +15,53 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  next();
+});
+
+app.use((req, res, next) => {
+  const ext = path.extname(req.path);
+  switch (ext) {
+    case '.html':
+      res.setHeader('Content-Type', 'text/html');
+      break;
+    case '.css':
+      res.setHeader('Content-Type', 'text/css');
+      break;
+    case '.js':
+      res.setHeader('Content-Type', 'text/javascript');
+      break;
+    case '.png':
+      res.setHeader('Content-Type', 'image/png');
+      break;
+    // Add more cases as needed
+    default:
+      break;
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, '../public')));
+
+// Route for CSS.
+app.get('/css/styles.css', (req, res) => {
+  // res.setHeader('Content-Type', 'text/css');
+  res.sendFile(path.join(__dirname, '../public/css/styles.css'));
+});
+
+// Route for JS.
+app.get('/js/index.js', (req, res) => {
+  // res.setHeader('Content-Type', 'text/javascript');
+  res.sendFile(path.join(__dirname, '../public/script.js'));
+});
+
+// Route for IMAGES.
+app.get('/images/:imageName', (req, res) => {
+  const imageName = req.params.imageName;
+  // res.setHeader('Content-Type', 'image/png');
+  res.sendFile(path.join(__dirname, `../public/images/${imageName}`));
+});
 
 app.use('/', indexRouter);
 
