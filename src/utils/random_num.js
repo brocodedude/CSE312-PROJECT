e204906daec803129d17841064a3daf4ab38d2b6
RA341 <razerishi1505@gmail.com>
2024-03-07 12:21:22 -0500

    added auth middleware and random num gen

---
+++ src/utils/random_num.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {number} length
+ * @returns {string}
+ */
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+module.exports = generateRandomString
