5dbf31b031dd028fdde99baa5da6ecc1cfadfd5e
RA341 <razerishi1505@gmail.com>
2024-03-09 16:01:59 -0500

    inital game

---
+++ src/game/game.controller.js
@@ -0,0 +1,82 @@
+const {io} = require('../server')
+const express = require("express");
+const {v4: uuidv4} = require("uuid");
+
+// servies
+const {activeLobbies, joinLobby} = require('./game.service')
+const insertValuesInHTML = require('../utils/html_inserter')
+const {getLobbyId} = require("../lobby/lobby.service");
+const {handlePosMsg, handlePelletMsg, handleJoinMsg, handleDisconnect} = require('./game.websocket')
+
+const router = express.Router();
+
+router.get('/play', async (req, res, next) => {
+    console.log('Received join lobby request')
+    // check auth and verify user
+    let userId = '';
+    let lobbyId = ''
+    try {
+        const queryParams = req.url.split('?')[1].split('&')
+        userId = queryParams[0].split('=')[1]
+        lobbyId = queryParams[1].split('=')[1]
+    } catch (e) {
+        console.log('Failed to read query prams')
+        console.log(e)
+        res.status(403).send('Make sure the correct parms are passed')
+    }
+    // generate a tmp user id
+    const playerTmpUUid = uuidv4()
+
+    try {
+        const tmp = await getLobbyId(lobbyId)
+        const lobbyUUId = tmp[0]['lobby_id']
+
+        if (lobbyUUId === undefined) {
+            throw Error('Failed to get lobbyId, Probably incorrect id')
+        }
+
+        const result = activeLobbies[lobbyUUId].join(playerTmpUUid)
+
+        // if failed to join lobby because its full or something
+        if (!result) {
+            res.status(403).send('Lobby is full try another lobby or you are unauthorized')
+        }
+
+        // insert lobbyid and playerid in html
+        const html = await insertValuesInHTML(
+            './public/game/game.html',
+            lobbyUUId,
+            playerTmpUUid
+        )
+
+        res.set('Content-Type', 'text/html');
+        res.send(html);
+    } catch (e) {
+        console.log(e)
+        res.status(403).send('Something went wrong, contact web admins')
+    }
+})
+
+
+io.on('connection', (socket) => {
+    socket.on('join', (msg) => {
+        return handleJoinMsg(msg, socket)
+    })
+
+    // handle player pos events
+    socket.on('pos', (msg) => {
+        return handlePosMsg(msg, socket)
+    })
+
+    // handle player eating pellets events
+    socket.on('pellet', (msg) => {
+        return handlePelletMsg(msg, socket)
+    });
+
+    // handle disconnect
+    socket.on('disconnect', (reason) => {
+        return handleDisconnect(reason, socket)
+    });
+})
+
+module.exports = router
---
+++ src/game/player.model.js
@@ -0,0 +1,21 @@
+class Player {
+    /**
+     *
+     * @param {string|null} commandType
+     * @param {string} playerId
+     * @param {string} user
+     * @param {string} spriteType
+     * @param {string} x
+     * @param {string} y
+     */
+    constructor(commandType, playerId, user, spriteType, x, y) {
+        this.commandType = commandType;
+        this.playerid = playerId;
+        this.user = user;
+        this.spriteType = spriteType;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+module.exports = Player
