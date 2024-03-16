8cbf976025e7217565a0fdf1fd46767a3d8478a4
sciencestorm <pieohwhy@gmail.com>
2024-03-16 19:40:38 -0400

    Create lobby done, just needs JS functionality, only HTML is confirmed working.

---
+++ public/lobby_files/lobby_scripts.js
@@ -0,0 +1,15 @@
+const insertLobbies = () => {
+    //This function inserts the lobby HTML text
+    //We replace {{lobbies}} with HTML of all given lobbies
+}
+
+const createLobby = () => {
+    //This Function creates the lobby when a name is inserted
+    //and the user presses the create button
+    const lobby_name = document.getElementById("lobby-text-box").value
+    //ADD UID HERE AND SEND POST
+}
+
+const logout = () => {
+    //This function is run when logout button is pressed
+}
