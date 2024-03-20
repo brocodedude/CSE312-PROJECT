ff2bde4c52f6a91d4144d98d84c0f4bb064b26cb
sciencestorm <pieohwhy@gmail.com>
2024-03-20 04:55:27 -0400

    Added borders and Create and Delete functions

--- public/lobby_files/lobby_scripts.js
+++ public/lobby_files/lobby_scripts.js
@@ -7,12 +7,11 @@ async function fetchLobbies() {
         const lobbies = await response.json();
 
         // Extracting names and IDs from lobbies
-        const lobbiesInfo = lobbies.map(lobby => ({
+        return lobbies.map(lobby => ({
             id: lobby.id,
             name: lobby.lobby_name
         }));
 
-        return lobbiesInfo;
     } catch (error) {
         console.error('Error fetching lobbies:', error);
         return [];
@@ -22,7 +21,28 @@ async function fetchLobbies() {
 const createLobby = () => {
     //This Function creates the lobby when a name is inserted
     //and the user presses the create button
-    const lobby_name = document.getElementById("lobby-text-box").value
+    const lobby_name = document.getElementById("lobby-text-box").value;
+    const lobbyData = {
+        lobby_name: lobby_name,
+        uid: "1"    //REPLACE WITH UID OF USER WHO CREATED IT
+    }
+
+    fetch('/api/lobby', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(lobbyData)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to create lobby');
+            }
+            console.log('Lobby Created')
+        })
+        .catch(error => {
+            console.error('Error creating lobby', error);
+        })
 }
 
 const logout = () => {
@@ -36,6 +56,17 @@ const editLobby = () => {
 
 }
 
-const deleteLobby = () => {
-
+const deleteLobby = (lobbyID) => {
+    fetch(`/api/lobby/${lobbyId}`, {
+        method: 'DELETE',
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to delete lobby');
+            }
+            console.log('Lobby deleted successfully');
+        })
+        .catch(error => {
+            console.error('Error deleting lobby:', error);
+        });
 }
