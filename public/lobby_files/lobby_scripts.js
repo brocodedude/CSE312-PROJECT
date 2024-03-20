62a3e427bc9dbf62ee488abc4ef53d536d311080
sciencestorm <pieohwhy@gmail.com>
2024-03-19 22:07:04 -0400

    Lobby is now working with hardcoded test lobbies

--- public/lobby_files/lobby_scripts.js
+++ public/lobby_files/lobby_scripts.js
@@ -1,15 +1,41 @@
-const insertLobbies = () => {
-    //This function inserts the lobby HTML text
-    //We replace {{lobbies}} with HTML of all given lobbies
+async function fetchLobbies() {
+    try {
+        const response = await fetch('/api/lobby');
+        if (!response.ok) {
+            throw new Error('Failed to fetch lobbies');
+        }
+        const lobbies = await response.json();
+
+        // Extracting names and IDs from lobbies
+        const lobbiesInfo = lobbies.map(lobby => ({
+            id: lobby.id,
+            name: lobby.lobby_name
+        }));
+
+        return lobbiesInfo;
+    } catch (error) {
+        console.error('Error fetching lobbies:', error);
+        return [];
+    }
 }
 
 const createLobby = () => {
     //This Function creates the lobby when a name is inserted
     //and the user presses the create button
     const lobby_name = document.getElementById("lobby-text-box").value
-    //ADD UID HERE AND SEND POST
 }
 
 const logout = () => {
     //This function is run when logout button is pressed
+    const del ={method: "POST"}
+    const delReq = new Request("../logout")
+    fetch(delReq, del).then((response) => {})
+}
+
+const editLobby = () => {
+
+}
+
+const deleteLobby = () => {
+
 }
